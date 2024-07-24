4988790fb1d7b2c90c2736b2a0dde6cad1eea745
Blake <121067208+Blake-Peterson@users.noreply.github.com>
2024-07-24 14:43:23 -0500

    added current layout of css, still need clear button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let first_num;
 let operator;
 let second_num;
+const container = document.querySelector("#container");
 
 function add(num1, num2){
     return num1+num2;
@@ -30,3 +31,54 @@ function operate(first_num,operation, second_num){
     }
     return result;
 }
+
+function create_display(){
+    const display = document.createElement("div");
+    display.setAttribute("id","display");
+    container.appendChild(display);
+}
+
+function create_calculator_button_layout(){
+    let symbol="";
+    for(let i=0;i<4;i++){
+        const rows = document.createElement("div");
+        container.appendChild(rows);
+        for(let j=0;j<4;j++){
+            const buttons = document.createElement("button");
+            let number = (i*4)+j;
+            if(number>9){
+                switch(number){
+                    case 10:
+                        symbol="+";
+                        break;
+                    case 11:
+                        symbol="-";
+                        break;
+                    case 12:
+                        symbol="X";
+                        break;
+                    case 13:
+                        symbol="/";
+                        break;
+                    case 14:
+                        symbol=".";
+                        break;
+                    case 15:
+                        symbol="=";
+                        break;
+                }
+                buttons.textContent = symbol;
+            } else{
+                buttons.textContent = number.toString();
+            }
+            rows.appendChild(buttons);
+        }
+    }
+}
+
+function create_calculator(){
+    create_display();
+    create_calculator_button_layout();
+}
+
+create_calculator();
