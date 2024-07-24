f0e6f730f7ee8f72bf576eb49a394d6fed060e61
Blake <121067208+Blake-Peterson@users.noreply.github.com>
2024-07-24 13:37:45 -0500

    create script tag and insert operate function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let first_num;
+let operator;
+let second_num;
+
 function add(num1, num2){
     return num1+num2;
 }
@@ -12,4 +16,17 @@ function multiply(num1,num2){
 
 function divide(num1,num2){
     return num1/num2;
+}
+
+function operate(first_num,operation, second_num){
+    if (operation === "+"){
+        result=add(first_num,second_num);
+    } else if (operation === "-"){
+        result=subtract(first_num,second_num);
+    } else if (operation === "*"){
+        result=multiply(first_num,second_num);
+    } else if(operation === "/"){
+        result=divide(first_num,second_num);
+    }
+    return result;
 }
